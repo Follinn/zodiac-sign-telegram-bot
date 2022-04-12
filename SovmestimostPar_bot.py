a61bd7b2ec175bf1d48ba52e3c4eeb6a71f5ee5c
Follinn <102748618+Follinn@users.noreply.github.com>
2022-04-12 18:09:26 +0300

    Update SovmestimostPar_bot.py

--- SovmestimostPar_bot.py
+++ SovmestimostPar_bot.py
@@ -367,8 +367,8 @@ async def ifif(message : types.Message):
 
 
  if __name__ == '__SovmestimostPar_bot__':
-    logging.basicConfig(level=logging.INFO)
-    start_webhook(
+     logging.basicConfig(level=logging.INFO)
+     start_webhook(
         dispatcher=dp,
         webhook_path=WEBHOOK_PATH,
         skip_updates=True,
@@ -376,4 +376,4 @@ async def ifif(message : types.Message):
         on_shutdown=on_shutdown,
         host=WEBAPP_HOST,
         port=WEBAPP_PORT,
-    )
+     )
